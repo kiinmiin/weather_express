170a224d8cc1b00584850b1d108eec7e49828c8a
kiinmiin <rasmo.kiin@voco.ee>
2024-09-23 11:46:51 +0300

    Created form request response with API data

--- index.js
+++ index.js
@@ -6,10 +6,31 @@ const fetch = require('node-fetch')
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
+app.use(express.json())
+app.use(express.urlencoded({extended : true}))
+
 const key = 'd9d9aceeb82d442e82280102242309';
 let city = 'Tallinn'
 
 app.get('/', function (req, res) {
+    fetch(`http://api.weatherapi.com/v1/current.json?key=${key}&q=${city}&agi=no`)
+    .then((response) => {
+        return response.json()
+    })
+    .then((data) => {
+        let condition = data.current.condition.text
+        let city = data.location.name
+        let temp = data.current.temp_c
+        res.render('index', {
+            condition: condition,
+            city: city,
+            temp: temp
+        })
+    })
+})
+
+app.post('/', function(req, res) {
+    let city = req.body.cityname
     fetch(`http://api.weatherapi.com/v1/current.json?key=${key}&q=${city}&agi=no`)
     .then((response) => {
         return response.json()
