2480677917a3ca77f00f924755014dd851a9abc8
kiinmiin <rasmo.kiin@voco.ee>
2024-09-23 11:29:17 +0300

    Added API data to project template file

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
 const express = require('express')
 const app = express()
-
 const path = require('path')
+const fetch = require('node-fetch')
 
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
+const key = 'd9d9aceeb82d442e82280102242309';
+let city = 'Tallinn'
+
 app.get('/', function (req, res) {
-    res.render('index')
+    fetch(`http://api.weatherapi.com/v1/current.json?key=${key}&q=${city}&agi=no`)
+    .then((response) => {
+        return response.json()
+    })
+    .then((data) => {
+        let condition = data.current.condition.text
+        let city = data.location.name
+        let temp = data.current.temp_c
+        res.render('index', {
+            condition: condition,
+            city: city,
+            temp: temp
+        })
+    })
 })
 
 app.listen(3000)
